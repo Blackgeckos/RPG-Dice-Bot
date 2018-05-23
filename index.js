64dd127fc5a11b72730b9082e164f61ca56bc5af
Blackgeckos <arnaudlaurat@gmail.com>
2018-05-23 18:45:19 +0200

    Version 1.0 Add all files to try to host Bot 24/7 on Heroku

---
+++ index.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const Bot = new Discord.Client();
+
+Bot.login('NDQ4NTk0MjMzMzE0NTA4ODIx.Deb9rw.9TqEyFhs8xtFDkL1gPFUQmSVmUU');
+
+Bot.on('ready', function() {
+    Bot.user.setActivity('Antika').catch(console.error)
+})
+
+
+
+Bot.on('message', function(message) {
+    if (message.content === '!ping') {
+        message.reply('pong')
+    }
+
+const args = message.content.substring().split(" ");
+
+    switch (args[0].toLowerCase()) {
+        case "!d":
+            var d = Math.floor(Math.random() * args[1] )+1;
+            if (!d) return message.reply ("Quel dé dois-je lancer ?");
+            message.reply("Résultat du lancé: "+d);
+            break;
+    }
+});
